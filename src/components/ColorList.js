720716a430ec6ed9a82ed2f79a061d9e6507344c
github username <github email address>
2021-02-01 14:07:37 -0500

    done

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -7,18 +7,65 @@ function ColorList() {
     "salmon",
     "darkslategray",
     "hotpink",
+    "blue"
   ];
+  // To fix this error, we must give each <li> element a special key prop, like so:
+  // This special key prop allows React internally to keep track of each element in the array of JSX
+  const colorElements = colors.map((color) => {
+    return <li key={color} style={{ color:color}}>{color}</li>;
+  })
 
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
-      <li style={{ color: colors[0] }}>{colors[0]}</li>
-      <li style={{ color: colors[1] }}>{colors[1]}</li>
-      <li style={{ color: colors[2] }}>{colors[2]}</li>
-      <li style={{ color: colors[3] }}>{colors[3]}</li>
-      <li style={{ color: colors[4] }}>{colors[4]}</li>
+        <ol>
+          {colorElements}
+        </ol>
     </div>
   );
 }
 
 export default ColorList;
+
+// key prop with Object, use the ID as the key prop
+
+// const userHeadings = users.map((user) => {
+//   return <h1 key={user.id}>{user.firstName}</h1>;
+// });
+
+
+
+// With Non-Unique Arrays, use the index for the key properties, use as last resort only
+
+// const fib = [0, 1, 1, 2, 3, 5];
+
+// const fibList = fib.map((number, index) => {
+//   return <div key={index}>{number}</div>;
+// });
+
+
+
+
+// With Nested Components
+
+// ColorItem component
+// function ColorItem(props) {
+//   return <li style={{ color: props.color }}>{props.color}</li>;
+// }
+
+// ColorList component
+// function ColorList() {
+//   const colors = [
+//     "firebrick",
+//     "rebeccapurple",
+//     "salmon",
+//     "darkslategray",
+//     "hotpink",
+//   ];
+
+//   const colorElements = colors.map((color) => {
+//     return <ColorItem key={color} color={color} />;
+//   });
+//   // etc
+// } 
+
